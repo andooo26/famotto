3098cdf16d9bd5d474f74f9d1bba956ba8a49a3f
andooo26 <123529443+andooo26@users.noreply.github.com>
2025-12-17 10:58:08 +0900

    フッターの改善

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,6 +6,10 @@ import Link from 'next/link';
 export default function Footer() {
   return (
     <footer className="footer">
+      <Link href="/">
+        <Image src="/home.png" alt="" width={60} height={60} />
+        <span>ホーム</span>
+      </Link>
       <Link href="/diary">
         <Image src="/add.png" alt="" width={60} height={60} />
         <span>日記追加</span>
@@ -17,6 +21,10 @@ export default function Footer() {
       <Link href="/menu">
         <Image src="/menu.png" alt="" width={60} height={60} />
         <span>日記確認</span>
+      </Link>
+      <Link href="/option">
+        <Image src="/option.png" alt="" width={60} height={60} />
+        <span>設定</span>
       </Link>
     </footer>
   );
